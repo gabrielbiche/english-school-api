ca456dc7d96886400efefd70d86dcacd28d752e2
GabrielBiche <gabrielsbiche@gmail.com>
2022-01-06 15:15:31 -0300

    implemented table relation in levels

--- src/models/levels.js
+++ src/models/levels.js
@@ -1,23 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Levels extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Levels.hasMany(models.Clazzes, { foreignKey: 'level_id'})
     }
-  };
-  Levels.init({
-    level_description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Levels',
-  });
-  return Levels;
-};
+  }
+  Levels.init(
+    {
+      level_description: DataTypes.STRING
+    },
+    {
+      sequelize,
+      paranoid: 'true',
+      modelName: 'Levels'
+    }
+  )
+  return Levels
+}
