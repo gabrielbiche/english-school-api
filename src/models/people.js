14ec4a33ac23598faed1cf04f1004c59630c037a
GabrielBiche <gabrielsbiche@gmail.com>
2022-01-06 15:18:05 -0300

    implemented table relation in people

--- src/models/people.js
+++ src/models/people.js
@@ -1,26 +1,49 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class People extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      People.hasMany(models.Registrations, {
+        foreignKey: 'student_id',
+        scope: { status: 'active' },
+        as: 'enrolledClazzes'
+      })
+      People.hasMany(models.Clazzes, { foreignKey: 'teacher_id' })
     }
-  };
-  People.init({
-    name: DataTypes.STRING,
-    active: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'People',
-  });
-  return People;
-};
+  }
+  People.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [2, 10]
+        }
+      },
+      active: DataTypes.BOOLEAN,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Invalid e-mail data type'
+          }
+        }
+      },
+      role: DataTypes.STRING
+    },
+    {
+      defaultScope: {
+        where: {
+          active: true
+        }
+      },
+      scopes: {
+        all: { where: {} }
+      },
+      sequelize,
+      paranoid: 'true',
+      modelName: 'People'
+    }
+  )
+  return People
+}
