de56e973e6526e3fbd799988cd44f38c0afd57b7
GabrielBiche <gabrielsbiche@gmail.com>
2022-01-06 21:28:42 -0300

    added classes services

---
+++ src/services/ClazzesServices.js
@@ -0,0 +1,9 @@
+const Services = require('./Services')
+
+class ClazzesServices extends Services {
+  constructor() {
+    super('Clazzes')
+  }
+}
+
+module.exports = ClazzesServices
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const PeopleServices = require('./PeopleServices')
 const LevelsServices = require('./LevelsServices')
 const RegistrationsServices = require('./RegistrationsServices')
+const ClazzesServices = require('./ClazzesServices')
 
 module.exports = {
   PeopleServices: PeopleServices,
   LevelsServices: LevelsServices,
-  RegistrationsServices: RegistrationsServices
+  RegistrationsServices: RegistrationsServices,
+  ClazzesServices: ClazzesServices
 }
