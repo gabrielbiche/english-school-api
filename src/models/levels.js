df91aa5f8dff8e4012bf1995c702c20a2f77a3e4
GabrielBiche <gabrielsbiche@gmail.com>
2022-01-10 08:30:52 -0300

    added validation of model level

--- src/models/levels.js
+++ src/models/levels.js
@@ -1,14 +1,32 @@
 'use strict'
+
 const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
   class Levels extends Model {
     static associate(models) {
-      Levels.hasMany(models.Clazzes, { foreignKey: 'level_id'})
+      Levels.hasMany(models.Clazzes, { foreignKey: 'level_id' })
     }
   }
   Levels.init(
     {
-      level_description: DataTypes.STRING
+      level_description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          is: {
+            args: /[^\d]/,
+            msg: 'The level description must be expressed in string only.'
+          },
+          notEmpty: {
+            msg: 'Please enter the level description.'
+          },
+          len: {
+            args: [4, 20],
+            msg: 'The level description must contain between 4 and 20 characters.'
+          }
+        }
+      }
     },
     {
       sequelize,
