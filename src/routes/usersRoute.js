858a11698bdd99916fc0b58d73a4d232950cf643
GabrielBiche <gabrielsbiche@gmail.com>
2022-01-20 11:40:19 -0300

    added users entity

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,11 +1,25 @@
 const { Router } = require('express')
 
 const UserController = require('../controllers/UserController')
+const { authenticationMiddleware } = require('../middlewares')
 
 const router = Router()
 
-router.post('/users', UserController.create)
-router.post('/users/login')
-router.delete('/users/:id')
+router.post('/users/signup', UserController.create)
+router.post(
+  '/users/login',
+  authenticationMiddleware.local,
+  UserController.login
+)
+router.get(
+  '/users/logout',
+  authenticationMiddleware.bearer,
+  UserController.logout
+)
+router.delete(
+  '/users/:id',
+  authenticationMiddleware.bearer,
+  UserController.delete
+)
 
 module.exports = router
